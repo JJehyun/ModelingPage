49e346ce7ae32e9a9fd26c012f1c26cad8d63c01
JJehyun <povt99@gmail.com>
2022-11-19 19:02:29 +0900

    2022/11/19 모델링 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef , useEffect} from 'react';
 import './App.css';
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    if (canvasRef.current) {
+      const scene = new THREE.Scene();
+      const renderer = new THREE.WebGLRenderer({
+        canvas: canvasRef.current,
+        antialias: true,
+      });
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  const camera = new THREE.PerspectiveCamera(30, 1);
+  camera.position.set(0, 0, 5);
+  const loader = new GLTFLoader();
+  scene.background = new THREE.Color("white");
+  const light = new THREE.DirectionalLight(0xffff00, 10);
+  scene.add(light);
+
+  loader.load("../../3DModeling/textures/scene.gltf", (object) => {
+    scene.add(object.scene);
+    renderer.render(scene, camera);
+  });
+}
+}, [canvasRef]);
+
+return <div><canvas ref={canvasRef} id="canvas" width="300" height="300"></canvas>;
+gkdlsd
+</div>
 }
 
 export default App;
